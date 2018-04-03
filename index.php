0d6c8efcd07130945cfd2ea434c0fb8832a36e01
김 영선 <deagool@nate.com>
2018-04-03 14:44:02 +0000

    1차 작업

--- index.php
+++ index.php
@@ -29,6 +29,17 @@
 		
 		<div class="container">
 			<div class="row">
+				<div class="card">
+				  <div class="card-header">
+					Portfolio
+				  </div>
+				  <div class="card-body">
+					<h5 class="card-title">사용기술</h5>
+					  <dt>서버</dt><dd>github page</dd>
+				  </div>
+				</div>
+			</div>
+			<div class="row">
 			  <div class="col-sm-6">
 				<div class="card">
 				  <div class="card-body">
@@ -55,6 +66,7 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
 		<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/vue"></script>
 		<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
   </body>
 </html>
